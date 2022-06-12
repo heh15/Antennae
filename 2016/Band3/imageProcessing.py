69b3960139ccacfb5bb6f8731ac5a99b65812592
heh15 <he_hao_@outlook.com>
2022-06-12 10:44:25 -0400

    Add the script to do basic CASA imaging manipulation

--- 2016/Band3/imageProcessing.py
+++ 2016/Band3/imageProcessing.py
@@ -4,18 +4,9 @@ impbcor(imagename='ngc40389overlap_band3_uvrange_robust_p5.image', pbimage='ngc4
 # exportfits image for band 3 robust 2.0 pbcor image
 exportfits(imagename='ngc40389overlap_band3_uvrange_robust_2_smooth.pbcor', fitsimage='ngc40389overlap_band3_uvrange_robust_2_smooth_pbcor.fits')
 
-# exportfits image for band 7 robust 2.0 pbcor image
-exportfits(imagename='ngc40389overlap_band7_range_robust_2_smooth.pbcor', fitsimage='ngc40389overlap_band7_range_robust_2_smooth_pbcor.fits')
 
 # exportfits image for band 3 pb
 exportfits(imagename='ngc40389overlap_band3_uvrange_robust_2.pb', fitsimage='ngc40389overlap_band3_uvrange_robust_2_pb.fits')
-
-# exportfits image for band 7 pb
-exportfits(imagename='ngc40389overlap_band7_range_robust_2.pb', fitsimage='ngc40389overlap_band7_range_robust_2_pb.fits')
-
-# primary beam correction for robust 0.5 band 7 image
-impbcor(imagename='ngc40389overlap_band7_range_robust_p5.image', pbimage='ngc40389overlap_band7_range_robust_p5.pb', 
-        outfile = 'ngc40389overlap_band7_range_robust_p5.pbcor')
 
 # smooth the band 3 image to the beam size of 0.95 arcsec
 imsmooth(imagename='ngc40389overlap_band3_uvrange_robust_p5.image', 
@@ -47,9 +38,6 @@ exportfits(imagename='ngc40389overlap_band3_uvrange_robust_p5_smooth_090.pbcor',
 exportfits(imagename='ngc40389overlap_band3_uvrange_robust_p5_smooth.pbcor',
            fitsimage='ngc40389overlap_band3_uvrange_robust_p5_smooth_pbcor.fits', overwrite=True)
 
-# export the Band 7 robust 0.5 smoothed image to fits file
-exportfits(imagename='ngc40389overlap_band7_range_robust_p5_smooth.pbcor', 
-           fitsimage='ngc40389overlap_band7_range_robust_p5_smooth_pbcor.fits', overwrite=True)
 
 # smooth the band 3 robust p5 image to the beam size of 0.134 arcsec 
 imsmooth(imagename='ngc40389overlap_band3_uvrange_robust_p5.image',
@@ -62,20 +50,19 @@ imsmooth(imagename='ngc40389overlap_band3_uvrange_robust_p5.image',
 impbcor(imagename='ngc40389overlap_band3_uvrange_robust_p5_smooth_134.image', pbimage='ngc40389overlap_band3_uvrange_robust_p5.pb',
         outfile='ngc40389overlap_band3_uvrange_robust_p5_smooth_134.pbcor', overwrite=True)
 
-# smooth the band 7 robust p5 image to the beam size of 0.134 arcsec
-imsmooth(imagename='ngc40389overlap_band7_range_robust_p5.image',
-         outfile='ngc40389overlap_band7_range_robust_p5_smooth_134.image',
-         major='0.134arcsec',
-         minor='0.134arcsec',
-         pa='0deg',
-         targetres=True,
-         overwrite=True)
-impbcor(imagename='ngc40389overlap_band7_range_robust_p5_smooth_134.image', pbimage='ngc40389overlap_band7_range_robust_p5.pb',
-        outfile = 'ngc40389overlap_band7_range_robust_p5_smooth_134.pbcor', overwrite=True)
 
+# smooth the band 3 robust 2 image to the beam size of 0.201 arcsec
+imsmooth(imagename='ngc40389overlap_band3_uvrange_robust_2_smooth.image', 
+        outfile='ngc40389overlap_band3_uvrange_robust_2_smooth_p201.image',
+        major='0.201arcsec',
+        minor='0.201arcsec',
+        pa='0deg',
+        targetres=True,
+        overwrite=True)
+impbcor(imagename='ngc40389overlap_band3_uvrange_robust_2_smooth_p201.image', pbimage='ngc40389overlap_band3_uvrange_robust_2.pb',
+        outfile='ngc40389overlap_band3_uvrange_robust_2_smooth_p201.pbcor', overwrite=True)
 
 # exportfits flat image for band 3 robust 2 image
 exportfits(imagename='ngc40389overlap_band3_uvrange_robust_2_smooth.image/', fitsimage='ngc40389overlap_band3_uvrange_robust_2_smooth.fits')
 
-# exportfits flat image for band 7 robust 2 image
-exportfits(imagename='ngc40389overlap_band7_range_robust_2_smooth.image', fitsimage='ngc40389overlap_band7_range_robust_2_smooth.fits')
+
--- 2016/Band7/imageProcessing.py
+++ 2016/Band7/imageProcessing.py
@@ -1,3 +1,43 @@
 exportfits(imagename='ngc40389overlap_band7_range_robust_2.image', fitsimage='ngc40389overlap_band7_range_robust_2.fits')
 
 exportfits(imagename='ngc40389overlap_band7_range_robust_2_smooth.image', fitsimage='ngc40389overlap_band7_range_robust_2_smooth.fits')
+
+# exportfits image for band 7 robust 2.0 pbcor image
+exportfits(imagename='ngc40389overlap_band7_range_robust_2_smooth.pbcor', fitsimage='ngc40389overlap_band7_range_robust_2_smooth_pbcor.fits')
+
+# exportfits image for band 7 pb
+exportfits(imagename='ngc40389overlap_band7_range_robust_2.pb', fitsimage='ngc40389overlap_band7_range_robust_2_pb.fits')
+
+# primary beam correction for robust 0.5 band 7 image
+impbcor(imagename='ngc40389overlap_band7_range_robust_p5.image', pbimage='ngc40389overlap_band7_range_robust_p5.pb', 
+        outfile = 'ngc40389overlap_band7_range_robust_p5.pbcor')
+
+# export the Band 7 robust 0.5 smoothed image to fits file
+exportfits(imagename='ngc40389overlap_band7_range_robust_p5_smooth.pbcor', 
+           fitsimage='ngc40389overlap_band7_range_robust_p5_smooth_pbcor.fits', overwrite=True)
+
+# exportfits flat image for band 7 robust 2 image
+exportfits(imagename='ngc40389overlap_band7_range_robust_2_smooth.image', fitsimage='ngc40389overlap_band7_range_robust_2_smooth.fits')
+
+# smooth the band 7 robust p5 image to the beam size of 0.134 arcsec
+imsmooth(imagename='ngc40389overlap_band7_range_robust_p5.image',
+         outfile='ngc40389overlap_band7_range_robust_p5_smooth_134.image',
+         major='0.134arcsec',
+         minor='0.134arcsec',
+         pa='0deg',
+         targetres=True,
+         overwrite=True)
+impbcor(imagename='ngc40389overlap_band7_range_robust_p5_smooth_134.image', pbimage='ngc40389overlap_band7_range_robust_p5.pb',
+        outfile = 'ngc40389overlap_band7_range_robust_p5_smooth_134.pbcor', overwrite=True)
+
+# smooth the band 7 robust 2 image to the beam size of 0.201 arcsec
+imsmooth(imagename='ngc40389overlap_band7_range_robust_2_smooth.image',
+        outfile='ngc40389overlap_band7_range_robust_2_smooth_p201.image',
+        major='0.201arcsec',
+        minor='0.201arcsec',
+        pa='0deg',
+        targetres=True,
+        overwrite=True)
+impbcor(imagename='ngc40389overlap_band7_range_robust_2_smooth_p201.image',pbimage='ngc40389overlap_band7_range_robust_2.pb',
+        outfile='ngc40389overlap_band7_range_robust_2_smooth_p201.pbcor',overwrite=True)
+
